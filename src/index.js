b6e96d63d1fb62df20bc39e042b71f8052d75168
Mac <mac@Macs-MacBook-Pro.local>
2017-06-24 19:39:57 +0700

    use new google map library

--- src/index.js
+++ src/index.js
@@ -1,9 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
-import SimpleMap from './SimpleMap';
 import registerServiceWorker from './registerServiceWorker';
 import './index.css';
 
-ReactDOM.render(<SimpleMap />, document.getElementById('root'));
+ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
