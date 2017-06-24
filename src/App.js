623232f8558661ed78fbcb3a449f5fba5268e79b
cntpjap <japyoyeaa@gmail.com>
2017-06-24 21:21:53 +0700

    add react router

--- src/App.js
+++ src/App.js
@@ -6,15 +6,8 @@ import GettingStartedGoogleMap from './GettingStartedGoogleMap';
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-        <div id="roots">
+      <div className="row">        
+        <div id="map-container" className="col-md-8 col-md-offset-2" style={{}}>
           <GettingStartedGoogleMap />
         </div>
       </div>
